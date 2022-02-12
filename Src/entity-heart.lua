65c93ee7870fd5a62f46d0ce35e945718e9d7086
hxr1117 <xr_1117@163.com>
2022-02-13 02:04:16 +0800

    Create entity-heart.lua

---
+++ Src/entity-heart.lua
@@ -0,0 +1,34 @@
+Heart = {}
+Heart.__index = Heart
+
+function Heart:Init()
+    local _heart = {
+        x = math.random(1, Display().x-2),
+        y = math.random(1, Display().y-2),
+        goal = false,
+        hole_position = {10, 10},
+    }
+    setmetatable(_heart, Heart)
+    return _heart
+end
+
+function Heart:UpdatePosition(x, y)
+    if (x < Display().x and y < Display().y) then
+        self.x = x
+        self.y = y
+    end
+end
+
+function Heart:Update(timeDelta)
+    local d_x = self.x - self.hole_position[1]
+    local d_y = self.y - self.hole_position[2]
+    if (math.abs(d_x) <= 2 and math.abs(d_y) <= 2) then
+        self.goal = true
+    end
+end
+
+function Heart:Draw()
+    DrawMetaSprite("hole", self.hole_position[1], self.hole_position[2])
+    DrawMetaSprite("heart", self.x, self.y)
+end
+
