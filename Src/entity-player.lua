5a678df1a60e3a564827ee46e7fecf2f434ca54a
Ciao-Wen-Chen <ogh478@gmail.com>
2022-02-12 21:03:39 +0000

    player update

--- Src/entity-player.lua
+++ Src/entity-player.lua
@@ -3,16 +3,17 @@
 Player = {}
 Player.__index = Player
 
-function Player:Init( px, py , spriteName )
+function Player:Init( px, py, spriteName, playerNum )
     
     --@ record bounds
     local display = Display()
     width = display.x
-    height = display.y
+    heigth = display.y
 
     --@ define the bounds of a player by the size of the sprite (X:width. Y:heigth)
     -- set all the variables
     local _player = {
+        playeridx = playerNum,
         hitRect = NewRect( px, py, SpriteSize().X, SpriteSize().Y ),
         metaSprite = MetaSprite( spriteName ), 
         alive = true,
@@ -21,7 +22,9 @@ function Player:Init( px, py , spriteName )
         frame = 1,
         speed = 1,
         blobSize = 20,
-        blobSpeed = 1
+        blobSpeed = 1,
+        dx = 0,
+        dy = 0
     }
 
     setmetatable(_player, Player)
@@ -31,37 +34,41 @@ end
 
 function Player:Update( timeDelta )
     
-    local dx = 0
-    local dy = 0
+    dx = self.dx
+    dy = self.dy
 
     -- If left is pressed, make the x velocity towards the left.
     if( Key( Keys.Left ) ) then
-        dx +  = -blobSpeed
+        self.dx = dx - self.blobSpeed
     end
 
     -- If right is pressed, make the x velocity towards the right.
     if( Key( Keys.Right ) ) then
-        dx +  = blobSpeed
+        self.dx = dx + self.blobSpeed
     end
 
     -- If up is pressed, make the y velocity upwards.
     if( Key( Keys.Up ) ) then
-        dy +  = -blobSpeed
+        self.dy = dy - self.blobSpeed
     end
 
     -- If down is pressed, make the y velocity downwards.
     if( Key( Keys.Down ) ) then
-        dy +  = blobSpeed
+        self.dy = dy + self.blobSpeed
     end
 
     -- calculating the net x and y velocities, 
     -- change the player's position accordingly.
-    self.blobX +  = dx
-    self.blobY +  = dy
+    self.blobX +  = self.dx
+    self.blobY +  = self.dy
+    self.hitRect.X + = self.dx
+    self.hitRect.Y + = self.dy
 
     -- restrict it within boundaries
     self.blobX % = width
-    self.blobY % = height
+    self.blobY % = heigth
+    self.hitRect.X % = dx
+    self.hitRect.Y % = dy
     
     -- Since the player is dead, there is nothing to do and we exit 
     -- out of the player's `LateUpdate()` function. This also insures 
